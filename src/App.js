3bcf127ce1a680bf4c91f67ec13fe890c4b8deee
NaveenDK <naveenkolambage@gmail.com>
2022-01-04 21:33:53 +1300

    added api request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import{MenuItem,FormControl,Select} from "@material-ui/core";
 import './App.css';
-import { findRenderedComponentWithType } from "react-dom/cjs/react-dom-test-utils.production.min";
+
 
 function App() {
+
+  const[countries,setCountries] = useState([])
+
+  useEffect(()=>{
+
+    const getCountriesData = async ()=>{
+     await fetch ("https://disease.sh/v3/covid-19/countries")
+     .then((response)=>response.json())
+     .then((data)=>{
+       const countries=data.map((country)=>({
+         name:country.country,
+         value:country.countryInfo.iso2
+       }))
+       setCountries(countries)
+     })
+    }
+    getCountriesData();
+  },[])
+
   return (
     <div className="App">
       <div className="app__header">
@@ -13,10 +32,12 @@ function App() {
             variant="outlined"
             value="abc"
             >
-              <MenuItem value="worldwide">Worldwide</MenuItem>
-              <MenuItem value="worldwide">Option 2</MenuItem>
-              <MenuItem value="worldwide">Option 3</MenuItem>
-              <MenuItem value="worldwide">Option 4</MenuItem>
+              {
+                countries.map(country=>(
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))
+
+              }
             </Select>
           </FormControl>
       </div>
