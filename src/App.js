14f62707062428ba341a8be69ce1c8704461192b
NaveenDK <naveenkolambage@gmail.com>
2022-01-04 21:05:21 +1300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
+import{MenuItem,FormControl,Select} from "@material-ui/core";
 import './App.css';
+import { findRenderedComponentWithType } from "react-dom/cjs/react-dom-test-utils.production.min";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__header">
+      <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+            <Select
+            variant="outlined"
+            value="abc"
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              <MenuItem value="worldwide">Option 2</MenuItem>
+              <MenuItem value="worldwide">Option 3</MenuItem>
+              <MenuItem value="worldwide">Option 4</MenuItem>
+            </Select>
+          </FormControl>
+      </div>
+
+ 
     </div>
   );
 }
